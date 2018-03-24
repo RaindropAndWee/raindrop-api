534efe611f0f24bc1c1491dc57a0a97c70ee0928
ptavarez16 <ptavarez16@yahoo.com>
2018-03-24 10:54:55 -0400

    Add product resource with index and create - Add product model with name, price description   - No relationship - Add product controller with working index and create methods - Add product routes for index and create - Add product shell scripts for index and create

---
+++ app/models/product.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    default: 'None Given'
+  }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
